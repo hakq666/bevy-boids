e57606f8f84f135ddfd75abfc4eccaa8a34f5d9d
hakq <hakq@tuta.io>
2023-07-12 11:43:37 +0200

    Setup window initialization

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::{PresentMode}};
+
+pub const WINDOW_TITLE: &str = "Bevy Boids";
+pub const WINDOW_WIDTH: f32 = 1280.0;
+pub const WINDOW_HEIGHT: f32 = 720.0;
 
 fn main() {
-    App::new().run();
+    App::new()
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window { 
+            present_mode: PresentMode::AutoVsync,
+            resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+            title: WINDOW_TITLE.into(),
+            ..default()
+         }),
+         ..default()
+    }))
+    .run();
 }
