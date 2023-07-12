c7f5269b6e0d51f25c57a2114ffdee830ca01b47
hakq <hakq@tuta.io>
2023-07-12 13:17:02 +0200

    Add movement systems

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use bevy::{prelude::*, window::{PresentMode}};
+mod movement;
+
+use bevy::prelude::*;
+use bevy::window::PresentMode;
+
+use crate::movement::{sync_position, update_position};
 
 pub const WINDOW_TITLE: &str = "Bevy Boids";
 pub const WINDOW_WIDTH: f32 = 1280.0;
@@ -6,14 +11,15 @@ pub const WINDOW_HEIGHT: f32 = 720.0;
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window { 
-            present_mode: PresentMode::AutoVsync,
-            resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
-            title: WINDOW_TITLE.into(),
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                present_mode: PresentMode::AutoVsync,
+                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                title: WINDOW_TITLE.into(),
+                ..default()
+            }),
             ..default()
-         }),
-         ..default()
-    }))
-    .run();
+        }))
+        .add_systems(Update, (update_position, sync_position).chain())
+        .run();
 }
